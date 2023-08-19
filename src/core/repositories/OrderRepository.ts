bdddd16842117e17127ee846aa202808a34b3b2f
Valentin Mendoza Torres <jmendozat13@gmail.com>
2023-08-19 10:05:25 -0500

    Added adm status by restaurant (#30)

--- src/core/repositories/OrderRepository.ts
+++ src/core/repositories/OrderRepository.ts
@@ -6,6 +6,7 @@ import { OrderStatus } from "@core/entities/OrderStatus";
 export interface OrderRepository {
     send(order: Order, restaurantCode: string): Promise<OrderStatus>
     status(userUUID: string): Promise<OrderStatus[]>
+    statusByRestaurant(restaurantCode: string): Promise<OrderStatus[]>
     findOne(orderId: string): Promise<OrderStatus>
     getOrderStates(): Promise<OrderState[]>
     updateOrderState(orderId: string, newOrderStateId: string): Promise<OrderStatus>
