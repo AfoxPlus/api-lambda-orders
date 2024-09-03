788bde8a07b32b416654d0ebd0c4897c73f8d95c
Valentin Mendoza <jmendozat13@gmail.com>
2024-09-03 12:59:17 -0500

    Added catch

--- src/functions/adm_state/handler.ts
+++ src/functions/adm_state/handler.ts
@@ -21,7 +21,7 @@ const sendState: ValidatedEventAPIGatewayProxyEvent<OrderUpdateRequest> = async 
 
     const title = "¡Buenas noticias, "+ result.client.name +"!"
     const body = "Tu pedido "+ result.number +" cambió de estado a "+ result.state+". Sigue el estado en tiempo real en nuestra app."
-    await orderRepository.sendOrderNotification(result.fcm_token, title, body)
+    await orderRepository.sendOrderNotification(result.fcm_token, title, body).catch(_ => {})
 
     return formatJSONSuccessResponse({
       success: true,
